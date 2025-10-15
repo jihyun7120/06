f9b17769b64e76a0f318afdc61133b4d10b8ce25
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-10-15 23:36:42 +0900

    practice 5

--- main.c
+++ main.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int square(int a)
+int factorial(int n)
 {
-   return (a*a);  
+    int res = 1;
+    int i;
+    for (i = 1; i<=n; i++)
+        res = res*i;
+    return res;
+}
+
+int combination(int n, int r)
+{
+    return (factorial(n)/factorial(n-r)/factorial(r));  
 }
 
 
 int main(int argc, char *argv[])
 
 {
-    int a = 2;
-    a = square(a);
-    printf("a=%i\n", a);
+    // varaible declare
+    int n;
+    int r;
+    int res;
+    
+    // input data
+    printf("input n and r:");
+    scanf("%d%d", &n, &r);
+    //compute combination
+    res = combination(n, r);
+    
+    //print result
+    printf("combination result is %i\n", res);
 
  system("PAUSE");
-
  return 0;
+ 
 
 }
