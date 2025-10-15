d342a3f1d91e05d36189e2375074cd9bb753cc49
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-10-15 18:50:30 +0900

    practice1

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printstar(int N)
+{
+     int i;
+     for(i=0; i<N; i++)
+         printf("*");
+}
+
+int main(int argc, char *argv[])
+
+{
+    printstar(10);
+    printstar(10);
+    printstar(10);
+
+
+
+
+ system("PAUSE");
+
+ return 0;
+
+}
